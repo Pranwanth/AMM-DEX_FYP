28cf9c29d3b3d58e9d7791e9910725026e2dc6d4
pranwanth <pranwanth@gmail.com>
2024-01-24 11:14:22 +0800

    fixed liquidityPoolToken test errors

--- backend/test/LiquidityPoolERC20TokenTest.ts
+++ backend/test/LiquidityPoolERC20TokenTest.ts
@@ -9,27 +9,42 @@ describe("LiquidityPoolERC20Token", function () {
   async function deployLiquidityPoolTokenFixture() {
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
-    const initialSupply = await ethers.parseUnits("1000", 18) // To create a initial supply of 1000 units
-    const lpTokenERC20 = await ethers.deployContract("LiquidityPoolToken", ["testToken", "TT", initialSupply]);
+    const initialSupply = ethers.parseUnits("1000", 18) // To create a initial supply of 1000 units
+    const lpTokenFactory = await ethers.getContractFactory("LiquidityPoolToken", owner)
+    const lpTokenERC20 = await lpTokenFactory.deploy(owner, "testToken", "TT", initialSupply)
 
     return { lpTokenERC20, owner, initialSupply, otherAccount };
   }
 
   describe("Deployment of LiquidityPoolERC20Token", function () {
-    it("Should set the right owner", async function () {
+    it("Should set the correct owner", async function () {
       const { lpTokenERC20, owner } = await loadFixture(deployLiquidityPoolTokenFixture);
       expect(await lpTokenERC20.owner()).to.equal(owner.address);
     });
 
+    it("Should set the correct name", async function () {
+      const { lpTokenERC20 } = await loadFixture(deployLiquidityPoolTokenFixture);
+      expect(await lpTokenERC20.name()).to.equal("testToken");
+    });
+
+    it("Should set the correct ticker", async function () {
+      const { lpTokenERC20 } = await loadFixture(deployLiquidityPoolTokenFixture);
+      expect(await lpTokenERC20.symbol()).to.equal("TT");
+    });
+
+    it("Should set the correct initialSupply", async function () {
+      const { lpTokenERC20, initialSupply } = await loadFixture(deployLiquidityPoolTokenFixture);
+      expect(await lpTokenERC20.totalSupply()).to.equal(initialSupply);
+    });
+
     it("Should mint the correct supply to the owner's address when deployed", async function () {
       const { lpTokenERC20, owner, initialSupply } = await loadFixture(deployLiquidityPoolTokenFixture);
-
       expect(await lpTokenERC20.balanceOf(owner.address)).to.equal(initialSupply);
     });
 
     it("Should mint the correct amount of tokens specified", async function () {
       const { lpTokenERC20, otherAccount } = await loadFixture(deployLiquidityPoolTokenFixture);
-      const valueToSend = await ethers.parseUnits("100", 18); // to mint 100 tokens to otherAccount
+      const valueToSend = ethers.parseUnits("100", 18); // to mint 100 tokens to otherAccount
       await lpTokenERC20.mint(otherAccount.address, valueToSend)
       expect(await lpTokenERC20.balanceOf(otherAccount.address)).to.equal(valueToSend)
     });
