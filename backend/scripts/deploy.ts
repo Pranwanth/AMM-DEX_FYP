62cc5be478098488883950d7b19e6f1b2ed19cfd
pranwanth <pranwanth@gmail.com>
2024-01-25 20:37:21 +0800

    deploy script wip

--- backend/scripts/deploy.ts
+++ backend/scripts/deploy.ts
@@ -1,22 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const factory = await ethers.deployContract("Factory")
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  await factory.waitForDeployment();
 
-  await lock.waitForDeployment();
+  const factoryAddress = await factory.getAddress()
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`Factory deployed at ${factoryAddress}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
