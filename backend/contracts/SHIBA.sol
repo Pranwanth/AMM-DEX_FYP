95c6d34ae5e7237949a9bb6e65ed0dd4f9f1dcb1
pranwanth <pranwanth@gmail.com>
2024-03-21 14:42:49 +0800

    added test ERC20 tokens

---
+++ backend/contracts/SHIBA.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SHIBA is ERC20 {
+  constructor() ERC20("SHIBA INU Token", "SHIBA") {
+    address trader = 0x02114A9f7f296cFADFB78FBd3fA769426ed6eb99;
+    _mint(msg.sender, 10000*10**18);
+    _mint(trader, 50*10**18);
+  }
+}
--- backend/ignition/modules/local_01.ts
+++ backend/ignition/modules/local_01.ts
@@ -5,5 +5,28 @@ export default buildModule("local_01", (m) => {
   const factory = m.contract("FactoryPool");
   const weth9 = m.contract("WETH9");
   const router = m.contract("Router", [deployedAddresses["local_01#FactoryPool"], deployedAddresses["local_01#WETH9"]])
-  return { factory, weth9, router };
+
+  // ERC20 Tokens for testing
+  const wBtc = m.contract("WBTC")
+  const link = m.contract("LINK")
+  const pepe = m.contract("PEPE")
+  const bnb = m.contract("BNB")
+  const matic = m.contract("MATIC")
+  const shiba = m.contract("SHIBA")
+  const arb = m.contract("ARB")
+  const okb = m.contract("OKB")
+
+  return {
+    factory,
+    weth9,
+    router,
+    wBtc,
+    link,
+    pepe,
+    bnb,
+    matic,
+    shiba,
+    arb,
+    okb
+  };
 });
