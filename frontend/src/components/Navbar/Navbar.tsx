cbb5daeeb3b5a0aad97aad85c4c7d9d1e8e28ce4
pranwanth <pranwanth@gmail.com>
2024-03-23 16:26:21 +0800

    moved navbar header styling to not affect btn

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 import ConnectWalletButton from "../ConnectWalletButton";
 const Navbar = () => {
   return (
-    <nav className="flex justify-between w-full px-8 py-10 text-primaryText font-bold">
-      <div className="flex gap-8 text-xl">
+    <nav className="flex justify-between w-full px-8 py-10">
+      <div className="flex gap-8 text-xl text-primaryText font-bold">
         <button>
           <Link to="/">Swap</Link>
         </button>
