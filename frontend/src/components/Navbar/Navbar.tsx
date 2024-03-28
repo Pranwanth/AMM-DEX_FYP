58f21ac9133bbca2d971aa84f2b0997528cf66b5
pranwanth <pranwanth@gmail.com>
2024-03-28 15:47:30 +0800

    added route back to homepage when clicking SkySwap

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -4,15 +4,11 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between w-full px-8 py-3 bg-white">
       <div className="flex gap-8 text-xl text-primaryText font-bold items-center">
-        <div className="bg-gradient-to-r from-sky-400 via-sky-700 to-sky-950 inline-block text-transparent bg-clip-text">
+        <Link to="/" className="bg-gradient-to-r from-sky-400 via-sky-700 to-sky-950 inline-block text-transparent bg-clip-text">
           SkySwap
-        </div>
-        <button>
-          <Link to="/">Swap</Link>
-        </button>
-        <button>
-          <Link to="/pool">Pool</Link>
-        </button>
+        </Link>
+        <Link to="/">Swap</Link>
+        <Link to="/pool">Pool</Link>
       </div>
       <ConnectWalletButton className="w-40 p-2" />
     </nav>
