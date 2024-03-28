20650fb3e1861c2cb6300b714220463e0be62c6d
pranwanth <pranwanth@gmail.com>
2024-03-28 19:29:40 +0800

    added swap function

--- frontend/src/components/SwapActionButton.tsx
+++ frontend/src/components/SwapActionButton.tsx
@@ -1,14 +1,15 @@
 interface Props {
   className?: string;
+  handleClick: () => void
 }
 
 const SwapActionButton = (props: Props) => {
-  const { className } = props;
+  const { className, handleClick } = props;
   return (
     <button
-      className={`w-full text-center font-bold bg-sky-950 text-white p-4 rounded ${
-        className ?? ""
-      }`}
+      className={`w-full text-center font-bold bg-sky-950 text-white p-4 rounded ${className ?? ""
+        }`}
+      onClick={handleClick}
     >
       Swap
     </button>
--- frontend/src/pages/Swap.tsx
+++ frontend/src/pages/Swap.tsx
@@ -12,15 +12,20 @@ import SettingsPopoverContent from "../components/SettingsPopoverContent";
 import SwapActionButton from "../components/SwapActionButton";
 import SwapTokenButton from "../components/SwapTokenButton";
 import useSettingStore from "../store/useSettingStore";
+import { swapExactTokensForTokens } from "../components/utils/helper";
+import { approveERC20 } from "../components/utils/helper/ERC20";
+import { ROUTER_ADDR } from "../components/utils/ContractAdresses";
+import { createTokenApproveSuccessToastFromTx } from "../components/utils/toast";
 
 const Swap = () => {
-  const { isConnected } = useAccount();
+  const { isConnected, address: userAddress } = useAccount();
   const [openPopover, setOpenPopover] = useState(false);
   const [anchorElement, setAnchorElement] = useState<HTMLElement | null>(null);
   const [tokenZeroValue, setTokenZeroValue] = useState("");
   const [tokenOneValue, setTokenOneValue] = useState("");
   const [tokenZero, setTokenZero] = useState<Token | undefined>(undefined);
   const [tokenOne, setTokenOne] = useState<Token | undefined>(undefined);
+  const [approved, setApproved] = useState(false)
 
   const { swapSlippage } = useSettingStore();
 
@@ -83,6 +88,35 @@ const Swap = () => {
   //     }
   //   }
   // };
+  const handleApprove = async () => {
+    if (tokenZero && tokenZeroValue !== "") {
+      const tx = await approveERC20({
+        contractAddress: tokenZero.address,
+        spenderAddress: ROUTER_ADDR,
+        amount: tokenZeroValue,
+      }, false)
+      createTokenApproveSuccessToastFromTx([tx])
+      setApproved(true)
+    }
+    else {
+      return
+    }
+  }
+
+  const handleSwap = async () => {
+    if (tokenZero && tokenOne && tokenZeroValue !== "" && tokenOneValue !== "" && approved) {
+      await swapExactTokensForTokens(
+        tokenZeroValue,
+        "0",
+        [tokenZero.address, tokenOne.address],
+        userAddress as string,
+        Number.MAX_SAFE_INTEGER
+      )
+    }
+    else {
+      return
+    }
+  }
 
   return (
     <Card className="mt-12 mx-auto w-128 relative bg-gray-50 p-8 rounded-sm">
@@ -125,7 +159,19 @@ const Swap = () => {
         </div>
         <SwapTokenButton />
       </div>
-      {isConnected ? <SwapActionButton /> : <ConnectWalletButton />}
+      {!isConnected && <ConnectWalletButton />}
+      {isConnected && (
+        <>
+          {!approved
+            ?
+            <button className="connect-wallet-btn mt-5" onClick={handleApprove}>
+              Approve Token
+            </button>
+            :
+            <SwapActionButton handleClick={handleSwap} />
+          }
+        </>
+      )}
     </Card>
   );
 };
